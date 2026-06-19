7d47f6553a9fd8767366ff36fe301fc7c056968d
Tiorenn <ericorenanleal124@gmail.com>
2026-06-19 18:26:10 -0300

    refactor: improve create button locator using CSS and text selector

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -8,13 +8,13 @@ test('deve poder cadastrar uma nova tarefa utilizando Enter', async ({ page }) =
     await inputTaskName.press('Enter')
 });
 
-test('deve poder cadastrar uma nova tarefa utilizando o botão', async({ page }) => {
+test('deve poder cadastrar uma nova tarefa utilizando o botão', async ({ page }) => {
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[class*="InputNewTask"]')
     await inputTaskName.fill('Ler Um livro de Java')
-    const buttonNewTask = page.locator('xpath=//button[contains(text(),"Create")]')
-    await buttonNewTask.click()
+
+    await page.click('css=button >> text=Create')
 
     const deleteButtonTask = page.locator('button[class*="_listItemDeleteButton"]')
     await deleteButtonTask.last().click()
@@ -24,5 +24,5 @@ test('deve poder deletar uma tarefa', async ({ page }) => {
     await page.goto('http://localhost:8080')
     const deleteButtonsTask = page.locator('button[class*="_listItemDeleteButton"]')
     await deleteButtonsTask.last().click()
-    
+
 });
