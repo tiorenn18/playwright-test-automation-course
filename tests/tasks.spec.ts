4536ba5d3a3ed7126a0e62949563e62146ccb5b6
Tiorenn <ericorenanleal124@gmail.com>
2026-06-19 18:46:10 -0300

    test: add Faker for dynamic test data generation

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test('deve poder cadastrar uma nova tarefa utilizando Enter', async ({ page }) => {
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[class*="InputNewTask"]')
-    await inputTaskName.fill('ler Um livro de TypeScript')
+    await inputTaskName.fill(faker.lorem.words())
     await inputTaskName.press('Enter')
 });
 
@@ -12,7 +13,7 @@ test('deve poder cadastrar uma nova tarefa utilizando o botão', async ({ page }
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[class*="InputNewTask"]')
-    await inputTaskName.fill('Ler Um livro de Java')
+    await inputTaskName.fill(faker.lorem.words())
 
     await page.click('css=button >> text=Create')
 
