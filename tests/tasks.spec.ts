29cfeed75b76093341c1a5e04275963ceb3fe5bb
Tiorenn <ericorenanleal124@gmail.com>
2026-06-20 17:22:12 -0300

    test: add task checkbox validation

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -22,6 +22,19 @@ test('deve poder cadastrar uma nova tarefa utilizando o botão', async ({ page, 
     await page.click('css=button >> text=Create')
 });
 
+test('deve poder riscar uma tarefa', async ({ page, request }) => {
+    const taskName = ('Teste validado: checkBox funcionando')
+    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+    await page.goto('http://localhost:8080')
+
+    const inputTaskName = page.locator('input[class*="InputNewTask"]')
+    await inputTaskName.fill(taskName)
+    await page.click('css=button >> text=Create')
+
+    const checkBoxTask = page.locator('button[class*="_listItemToggle"]')
+    await checkBoxTask.last().click()
+});
+
 test('deve poder deletar uma tarefa', async ({ page }) => {
     await page.goto('http://localhost:8080')
 
