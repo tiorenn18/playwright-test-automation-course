240d69ce3f5b0d6ccb3c7ab2af11ed5c08978897
Tiorenn <ericorenanleal124@gmail.com>
2026-06-22 12:50:29 -0300

    chore: configure custom URLs with dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
 /// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
+require('dotenv').config()
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -32,7 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: true,
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.BASE_URL
   },
 
   /* Configure projects for major browsers */
