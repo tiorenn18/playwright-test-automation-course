653bba0d0d6db9872ebfe89e2c8360ad16052360
Tiorenn <ericorenanleal124@gmail.com>
2026-06-22 12:21:45 -0300

    test: organize task tests with describe block

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -4,78 +4,82 @@ import { deleteByTaskHelper, postTask } from './fixtures/support/helpers';
 import { TasksPage } from './fixtures/support/pages/tasks';
 import data from './fixtures/tasks.json'
 
-test('deve poder cadastrar uma nova tarefa utilizando Enter', async ({ page, request }) => {
-    const task = data.createTaskWithEnterValidation as TaskModel
+test.describe('cadastro', () => {
+    test('deve poder cadastrar uma nova tarefa utilizando Enter', async ({ page, request }) => {
+        const task = data.createTaskWithEnterValidation as TaskModel
 
-    const tasksPage: TasksPage = new TasksPage(page)
+        const tasksPage: TasksPage = new TasksPage(page)
 
-    await deleteByTaskHelper(request, task.name)
-    await tasksPage.goTo()
+        await deleteByTaskHelper(request, task.name)
+        await tasksPage.goTo()
 
-    await tasksPage.createTaskUsingEnter(task)
-    await tasksPage.shouldHaveText(task.name)
-});
+        await tasksPage.createTaskUsingEnter(task)
+        await tasksPage.shouldHaveText(task.name)
+    });
 
-test('deve poder cadastrar uma nova tarefa utilizando o botão', async ({ page, request }) => {
-    const task = data.createTaskWithButtonValidation as TaskModel
+    test('deve poder cadastrar uma nova tarefa utilizando o botão', async ({ page, request }) => {
+        const task = data.createTaskWithButtonValidation as TaskModel
 
-    const tasksPage: TasksPage = new TasksPage(page)
+        const tasksPage: TasksPage = new TasksPage(page)
 
-    await deleteByTaskHelper(request, task.name)
+        await deleteByTaskHelper(request, task.name)
 
-    await tasksPage.goTo()
-    await tasksPage.createTaskUsingButton(task)
-    await tasksPage.shouldHaveText(task.name)
-});
+        await tasksPage.goTo()
+        await tasksPage.createTaskUsingButton(task)
+        await tasksPage.shouldHaveText(task.name)
+    });
 
-test('deve concluir uma tarefa', async ({ page, request }) => {
-    const task = data.completeTaskCheckboxValidation as TaskModel
+    test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+        const task = data.duplicate as TaskModel
 
-    const tasksPage: TasksPage = new TasksPage(page)
+        const tasksPage: TasksPage = new TasksPage(page)
 
-    await deleteByTaskHelper(request, task.name)
-    await postTask(request, task)
+        await deleteByTaskHelper(request, task.name)
+        await postTask(request, task)
 
-    await tasksPage.goTo()
-    await tasksPage.shouldHaveText(task.name)
-    await tasksPage.validateTaskCheckbox(task.name)
-});
+        await tasksPage.goTo()
+        await tasksPage.shouldHaveText(task.name)
+        await tasksPage.createTaskUsingButton(task)
+        await tasksPage.alertHaveText('Task already exists!')
+    });
 
-test('deve poder deletar uma tarefa', async ({ page, request }) => {
-    const task = data.deleteTaskUsingButtonValidation as TaskModel
+    test('campo Obrigatório', async ({ page }) => {
+        const task = data.required as TaskModel
 
-    const tasksPage: TasksPage = new TasksPage(page)
+        const tasksPage: TasksPage = new TasksPage(page)
+        await tasksPage.goTo()
+        await tasksPage.createTaskUsingButton(task)
 
-    await deleteByTaskHelper(request, task.name)
-    await postTask(request, task)
+        const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+        expect(validationMessage).toEqual('This is a required field')
+    })
+})
 
-    await tasksPage.goTo()
+test.describe('atualizacão', () => {
+    test('deve concluir uma tarefa', async ({ page, request }) => {
+        const task = data.completeTaskCheckboxValidation as TaskModel
 
-    await tasksPage.shouldHaveText(task.name)
-    await tasksPage.validateDeleteTaskByButton(task.name)
-});
+        const tasksPage: TasksPage = new TasksPage(page)
 
-test('não deve permitir tarefa duplicada', async ({ page, request }) => {
-    const task = data.duplicate as TaskModel
+        await deleteByTaskHelper(request, task.name)
+        await postTask(request, task)
 
-    const tasksPage: TasksPage = new TasksPage(page)
+        await tasksPage.goTo()
+        await tasksPage.shouldHaveText(task.name)
+        await tasksPage.validateTaskCheckbox(task.name)
+    });
 
-    await deleteByTaskHelper(request, task.name)
-    await postTask(request, task)
+    test('deve poder deletar uma tarefa', async ({ page, request }) => {
+        const task = data.deleteTaskUsingButtonValidation as TaskModel
 
-    await tasksPage.goTo()
-    await tasksPage.shouldHaveText(task.name)
-    await tasksPage.createTaskUsingButton(task)
-    await tasksPage.alertHaveText('Task already exists!')
-});
+        const tasksPage: TasksPage = new TasksPage(page)
 
-test('campo Obrigatório', async ({ page }) => {
-    const task = data.required as TaskModel
+        await deleteByTaskHelper(request, task.name)
+        await postTask(request, task)
 
-    const tasksPage: TasksPage = new TasksPage(page)
-    await tasksPage.goTo()
-    await tasksPage.createTaskUsingButton(task)
+        await tasksPage.goTo()
 
-    const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
-    expect(validationMessage).toEqual('This is a required field')
+        await tasksPage.shouldHaveText(task.name)
+        await tasksPage.validateDeleteTaskByButton(task.name)
+    });
 })
